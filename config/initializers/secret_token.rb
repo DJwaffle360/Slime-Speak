1949fba0150933582a81035f0428e105c911dcaa
John Sebben <djwaffle360@gmail.com>
2013-09-01 08:19:30 -0500

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SlimeSpeak::Application.config.secret_token = 'e66e74775ecc29f0fdb7323ed8db556c1662c5394050ef953898018bd8827cfaee8e9364ba71c452d9255e6e16a5a468f49ca4d6714813f4512a6d4ca871195b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SlimeSpeak::Application.config.session_store :cookie_store, key: '_Slime_Speak_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SlimeSpeak::Application.config.session_store :active_record_store
